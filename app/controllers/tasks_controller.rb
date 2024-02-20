43d964d89f827228cfd51c49463625ffec99b240
Thomas Ellwood <taellwood@gmail.com>
2024-02-20 13:31:58 +0000

    find task by id created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,10 @@ def index
   @tasks = Task.all
 end
 
+def show
+  @task = Task.find(params[:id])
+end
+
 def new
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'tasks/index', do: 'tasks#index'
-  get 'tasks/new', do: 'tasks#new'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/new', to: 'tasks#new'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
