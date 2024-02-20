e830adb259349bfd9602a686f52f4948cb43af57
Thomas Ellwood <taellwood@gmail.com>
2024-02-20 17:07:31 +0000

    completed CRUD model for Task List

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks', to: 'tasks#index'
   # get 'tasks/:id', to: 'tasks#show'
 
   get 'tasks/new', to: 'tasks#new'
-  # post 'tasks', to: 'tasks#create'
+  post 'tasks', to: 'tasks#create'
+
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -13,4 +17,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
+
+
+#can use
+#resources :task
