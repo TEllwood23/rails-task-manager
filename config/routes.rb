d719b599d6ed8d3bbe9da72acfd7bc02cd5979fc
Thomas Ellwood <taellwood@gmail.com>
2024-02-20 14:10:15 +0000

    created link from show View back to Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :index
   # get 'tasks/:id', to: 'tasks#show'
   get 'tasks/:id', to: 'tasks#show', as: :task
   get 'tasks/new', to: 'tasks#new'
